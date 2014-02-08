61bf62145f18e742df011b75257c159c0852f2b4
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2014-02-08 11:20:40 +0200

    stick to the classic rendering

--- brot.py
+++ brot.py
@@ -1,13 +1,22 @@
-import random
+import sys
+
 from PIL import Image
 
 
-SIZE = 1024
+SIZE = 1023
 MAX_ITER = 100
 
 
-img = Image.new("RGB", (SIZE, SIZE))
+img = Image.new("RGB", (SIZE+1, SIZE+1))
 
+
+def carthesiza_2_complex(point):
+    """maps the 0-SIZE range to -2 1 (x axis) and -1.5 1.5 (y axis)"""
+    x, y = point
+    size = float(SIZE)
+    x = (-2 + (x/size)*3)
+    y = (-1.5 + (y/size)*3)
+    return complex(x, y)
 
 def complex_2_carthesian(point):
     x = int((point.real+2)/3 * SIZE)
@@ -15,36 +24,36 @@ def complex_2_carthesian(point):
     return x, y
 
 
-def render(orbit):
-    for point in orbit:
-        x, y  = complex_2_carthesian(point)
-        if (0 <= x < SIZE) and (0 <= y < SIZE):
-            pixel_value = img.getpixel((x, y))[0] + 1
-            img.putpixel((x, y), (pixel_value, pixel_value, pixel_value))
-
-
-def get_point():
-    return complex(-2+random.random()*3, -1+random.random()*2)
+def render(point):
+    # if (0 <= x < SIZE) and (0 <= y < SIZE):
+    x, y = complex_2_carthesian(point)
+    # pixel_value = img.getpixel((x, y))[0] + 1
+    img.putpixel((x, y), (255, 255, 255))
 
 
 def brot_orbit(point):
     iterations = 0
-    orbit = []
     z = point
-    while len(orbit) < MAX_ITER and z.imag**2 + z.real**2 < 4:
-        orbit.append(z)
+    while (iterations < MAX_ITER) and ((z.imag**2 + z.real**2) < 4):
         z = z**2 + point
-    return orbit
+        iterations += 1
+    return iterations
+
 
 def main():
-    try:
-        while 1:
-            point = get_point()
-            orbit = brot_orbit(point)
-            if len(orbit) == MAX_ITER:
-                render(orbit)
-    except KeyboardInterrupt:
-        img.save("brot.bmp", "BMP")
+    for x in xrange(SIZE +1):
+        for y in xrange(SIZE +1):
+            point = carthesiza_2_complex((x, y))
+            iterations = brot_orbit(point)
+            if iterations == MAX_ITER:
+                render(point)
+        #         sys.stdout.write(".")
+        #         sys.stdout.flush()
+        #     else:
+        #         sys.stdout.write(" ")
+        #         sys.stdout.flush()
+        # print
+    img.save("brot.bmp", "BMP")
 
 if __name__ == "__main__":
     main()
